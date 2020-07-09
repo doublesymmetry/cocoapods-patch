7063fb38759397963e65c40c7fdd4d9269648f23
David Chavez <david@dcvz.io>
2020-07-10 00:33:36 +0200

    Apply patches before generating xcode project

--- lib/cocoapods-patch/hook.rb
+++ lib/cocoapods-patch/hook.rb
@@ -3,16 +3,17 @@ require 'pathname'
 require_relative 'command/patch/apply'
 
 class Pod::Installer
-  # The last step cocoapods runs is perform_post_install_actions.
-  # We override it, run the original implementation and add our patches action
-  # Reference: https://github.com/CocoaPods/CocoaPods/blob/760828a07f8fcfbff03bce13f56a1789b6f5a95d/lib/cocoapods/installer.rb#L169
-  alias_method :perform_post_install_actions_old, :perform_post_install_actions
+  # Because our patches may also delete files, we need to apply them before the pod project is generated
+  # The project is generated in the `integrate` method, so we override it
+  # We first run our patch action and then the original implementation of the method
+  # Reference: https://github.com/CocoaPods/CocoaPods/blob/760828a07f8fcfbff03bce13f56a1789b6f5a95d/lib/cocoapods/installer.rb#L178
+  alias_method :integrate_old, :integrate
 
-  def perform_post_install_actions
-    # run the original implementation
-    perform_post_install_actions_old
-    # run custom apply_patches
+  def integrate
+    # apply our patches
     apply_patches
+    # run the original implementation
+    integrate_old
   end
 
   def apply_patches
