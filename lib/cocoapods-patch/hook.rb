5238d8c3ac3a9e9b098abdf40ce2a5753da776e6
David Chavez <david@dcvz.io>
2020-07-09 17:38:36 +0200

    Apply patches after the cocoapods process is done (#1)

--- lib/cocoapods-patch/hook.rb
+++ lib/cocoapods-patch/hook.rb
@@ -2,13 +2,23 @@ require 'cocoapods'
 require 'pathname'
 require_relative 'command/patch/apply'
 
-module CocoapodsPatch
-  module Hooks
-    Pod::HooksManager.register('cocoapods-patch', :post_install) do |context|
-      Pod::UI.puts 'Applying patches if necessary'
-      patches_dir = Pathname.new(context.sandbox_root) + '..' + 'patches'
-      patches = patches_dir.each_child.select { |c| c.to_s.end_with?('.diff') }
-      patches.each { |p| apply_patch(p) }
-    end
+class Pod::Installer
+  # The last step cocoapods runs is perform_post_install_actions.
+  # We override it, run the original implementation and add our patches action
+  # Reference: https://github.com/CocoaPods/CocoaPods/blob/760828a07f8fcfbff03bce13f56a1789b6f5a95d/lib/cocoapods/installer.rb#L169
+  alias_method :perform_post_install_actions_old, :perform_post_install_actions
+
+  def perform_post_install_actions
+    # run the original implementation
+    perform_post_install_actions_old
+    # run custom apply_patches
+    apply_patches
+  end
+
+  def apply_patches
+    Pod::UI.puts 'Applying patches if necessary'
+    patches_dir = Pathname.new(Dir.pwd) + 'patches'
+    patches = patches_dir.each_child.select { |c| c.to_s.end_with?('.diff') }
+    patches.each { |p| apply_patch(p) }
   end
 end
