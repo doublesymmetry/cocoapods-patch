f54f948d6f92d62e0c5fbb95c7a46671b1043519
David Chavez <david@dcvz.io>
2021-01-23 17:48:30 +0100

    Add test project

---
+++ spec/project/test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by David Chavez on 23.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
